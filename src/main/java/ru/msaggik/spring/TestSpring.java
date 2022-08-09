6f985719a46b0e9395d01e585816ad516b6ab979
MSagGik <108148690+MSagGik@users.noreply.github.com>
2022-08-09 17:20:37 +0300

    Update TestSpring.java

--- src/main/java/ru/msaggik/spring/TestSpring.java
+++ src/main/java/ru/msaggik/spring/TestSpring.java
@@ -1,1 +1,17 @@
-package ru.msaggik.spring;import org.springframework.context.annotation.AnnotationConfigApplicationContext;import ru.msaggik.spring.config.SpringConfig;import ru.msaggik.spring.music.ClassicalMusic;public class TestSpring {    public static void main(String[] args) {        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);        System.out.println(musicPlayer.playMusic());        context.close();    }}
+package ru.msaggik.spring;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.msaggik.spring.config.SpringConfig;
+import ru.msaggik.spring.music.ClassicalMusic;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        System.out.println(musicPlayer.playMusic());
+
+        context.close(); 
+    }
+}
